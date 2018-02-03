99e2ea35d7dc76c4f92b50f422c28c9b3cedfcf1
Daniel Rozenzaft <drozenzaft@stuy.edu>
2018-02-02 22:40:12 -0500

    image code

---
+++ image.py
@@ -0,0 +1,11 @@
+ans = 'P3\n500 500\n255\n'
+for y in range(500):
+    for x in range(500):
+        r = y/2
+        g = x/2
+        b = (x+y)%255
+        ans += str(r) + ' ' + str(g) + ' ' + str(b) + ' '
+    ans += '\n'
+f = open('picmaker.png','w')
+f.write(ans)
+f.close()
